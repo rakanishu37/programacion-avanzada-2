c721e2986f48d0b81ad38ef61a4b4f2e05114911
fede <fede.3328@gmail.com>
2020-08-22 17:38:18 -0300

    Guia 05 completa

--- guia0.5/calculator.js
+++ guia0.5/calculator.js
@@ -1,42 +1,41 @@
-function sum(number1,number2) {
-    return number1 + number2;
+function add(number1, number2) {
+  return number1 + number2;
 }
 
-function substract(number1,number2) {
-    return number1 - number2;
+function substract(number1, number2) {
+  return number1 - number2;
 }
 
-function multiply(number1,number2) {
-    return number1 * number2;
+function multiply(number1, number2) {
+  return number1 * number2;
 }
 
-function divide(number1,number2) {
-    if(number2 === 0){
-        return undefined;
-    }
-    return number1 / number2;
+function divide(number1, number2) {
+  if (number2 === 0) {
+    return undefined;
+  }
+  return number1 / number2;
 }
 
 function calculator(operation, number1, number2) {
-    console.log(number1);
-    console.log(number2);
-    let result = 0;
-    switch(operation) {
-        case "sum": 
-            result = sum(number1, number2);
-            break;
-        
-        case "substract": 
-            result = substract(number1, number2);
-            break;
-        
-        case "multiply": 
-            result = multiply(number1, number2);
-            break;
-        
-        case "divide": 
-            result = divide(number1, number2);
-            break;
-    }
-    window.alert("Result: " + result);
+	let result = 0;
+	console.log(operation);
+  switch (operation) {
+    case "add":
+      result = add(number1, number2);
+      break;
+
+    case "substract":
+      result = substract(number1, number2);
+      break;
+
+    case "multiply":
+      result = multiply(number1, number2);
+      break;
+
+    case "divide":
+      result = divide(number1, number2);
+			break;
+	}
+	return result;
 }
